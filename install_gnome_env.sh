4ab5c913f72d38ec28ebfe36cc223a6362743758
John <john@sparta.pizza>
2020-11-07 20:23:50 +0100

    Fixing show application button in dock

--- install_gnome_env.sh
+++ install_gnome_env.sh
@@ -37,3 +37,5 @@ set_theme_opt 'clock-show-weekday' 'true'
 set_theme_opt 'enable-animations' 'true'
 set_theme_opt 'enable-hot-corners' 'false'
 set_theme_opt 'show-battery-percentage' 'false'
+
+gsettings set org.gnome.shell.extensions.dash-to-dock show-show-apps-button true
