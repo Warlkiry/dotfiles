a038550c0ee7dba72f7dacdea2db5bbf5cb1ed52
John <john@sparta.pizza>
2020-11-23 12:19:14 +0100

    Fix bug on nvim setup

--- install_tmux_neovim_env.sh
+++ install_tmux_neovim_env.sh
@@ -13,5 +13,5 @@ echo "source-file $STATIC_FILES_DIR/tmux_theme" | sudo tee -a /etc/tmux.conf
 echo $PROGRESS "Configuring neovim"
 #TODO   Fix neovim configuration & plugin installation system wide (for root too)
 sudo cp ./config.vim /usr/share/nvim/sysinit.vim
-sudo wget 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' -O /etc/vim/autoload/plug.vim
+sudo wget 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' -O /usr/share/nvim/runtime/autoload/plug.vim
 nvim --headless +PlugInstall +qa
