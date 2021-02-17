d335b20fd1d98aa2d3f6691c2b797375201ee866
John <john@john.pizza>
2021-02-17 17:12:02 +0100

    Setting alacritty as default terminal emulator

--- install_alacritty.sh
+++ install_alacritty.sh
@@ -6,16 +6,18 @@ echo $PROGRESS "Installing alacritty"
 
 sudo apt -y install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
 rustup override set stable
 rustup update stable
 
 export PATH=$HOME/.cargo/bin/:$PATH
 echo 'export PATH=$HOME/.cargo/bin/:$PATH' >> $HOME/.bashrc
-cd $STATIC_FILES_DIR
-sudo rm -rf alacritty
-sudo git clone https://github.com/alacritty/alacritty && cd alacritty
+
+curr_dir=$PWD
+cd $(mktemp -d)
+git clone https://github.com/alacritty/alacritty && cd alacritty
 cargo_path=$(which cargo)
-sudo $cargo_path build --release
+$cargo_path build --release
 
 sudo cp target/release/alacritty /usr/local/bin
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
@@ -29,4 +31,7 @@ mkdir -p ~/.bash_completion
 cp extra/completions/alacritty.bash ~/.bash_completion/alacritty
 echo "source ~/.bash_completion/alacritty" >> ~/.bashrc
 
-cp ./alacritty_config.yml $HOME/.alacritty.yml
+cp $curr_dir/alacritty_config.yml $HOME/.alacritty.yml
+
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator $(which alacritty) 50
+sudo update-alternatives --config x-terminal-emulator
